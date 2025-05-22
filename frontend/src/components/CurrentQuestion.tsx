e641b73c828c93077fb6e4bb5b3968a32bba9c99
Hh440 <harsh100045@gmail.com>
2025-05-22 16:59:11 +0530

    changes

---
+++ frontend/src/components/CurrentQuestion.tsx
@@ -0,0 +1,9 @@
+
+
+export const CurrentQuestion= ({question}:{question:any})=>{
+    return(
+        <div>
+            {JSON.stringify(question)}
+        </div>
+    )
+}
---
+++ frontend/src/components/LeaderBoard.tsx
@@ -0,0 +1,10 @@
+
+
+export const LeaderBoard=({leaderboard}:{leaderboard:any})=>{
+    return(
+        <div>
+            This is the leaderboard
+            {JSON.stringify(leaderboard)}
+        </div>
+    )
+}
